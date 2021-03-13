aaf1c2e0c02070c3eec52b20a8a800073d2d3fd3
tvQuan <tranvanquana1@gmail.com>
2021-03-13 13:16:35 +0700

    fix PORT

--- server.js
+++ server.js
@@ -6,4 +6,6 @@ app.get('/', function (req, res) {
       return res.json('Hello, world')
 })
 
-app.listen(3000, ()=> console.log('Server is running on Port: 3000'))
+const PORT = process.env.PORT || 80
+
+app.listen(PORT, ()=> console.log(`Server is running on Port: ${PORT}`))
